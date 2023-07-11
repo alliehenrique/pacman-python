928c029b50b9b84a3a167b14bcf94f67aa094102
Allie Henrique <127456382+alliehenrique@users.noreply.github.com>
2023-07-11 00:42:36 -0300

    Fonte Tela Inicial

--- constantes.py
+++ constantes.py
@@ -10,3 +10,11 @@ FPS = 30
 
 #Cores
 PRETO = (0, 0, 0)
+AMARELO = (244, 233, 51)
+
+#Imagens
+SPRITESHEET = 'spritesheet.png'
+PACMAN_START_LOGO = 'pacman-logo-1.png'
+
+#Fonte
+FONTE = 'times new roman'
